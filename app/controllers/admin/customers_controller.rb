4c7448504d9a4b9f54478c4ab2f1f7cde454f713
kanon118517 <124481925+kanon118517@users.noreply.github.com>
2023-05-02 02:48:20 +0000

    [Update]naganocake

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.all
   end
@@ -11,4 +11,17 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
+  end
+
 end
